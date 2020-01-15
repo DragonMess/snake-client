afc56dd03ed90353c95009e3728f0530c65c75ab
DragonMess <kmylop@hotmail.com>
2020-01-15 18:28:50 +0000

    import function like object

--- play.js
+++ play.js
@@ -1,24 +1,8 @@
 const net = require('net');
 
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: '192.168.0.103',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-
-  return conn;
-}
-module.exports = {connect};
-
-console.log('Connecting ...');
-connect();
-
 // play.js
 const { connect } = require('./client');
+
 console.log('Connecting ...');
 connect();
+
