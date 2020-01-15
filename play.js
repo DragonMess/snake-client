da2494bfef428935ddcb23991f48e22174591663
DragonMess <kmylop@hotmail.com>
2020-01-15 19:52:59 +0000

    message to server

--- play.js
+++ play.js
@@ -4,5 +4,17 @@ const net = require('net');
 const { connect } = require('./client');
 
 console.log('Connecting ...');
-connect();
 
+let conn = connect();
+//put return of fonction connect in conn
+
+
+conn.on('connect', (play) => {
+  console.log('Successfully connected to game server');
+  // client.write('Hello there!');// data for client
+});
+
+
+conn.on('connect', (play) => {
+  conn.write('Camilo');
+});
