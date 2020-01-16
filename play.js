ac3452c6275c291583cef5d2656aed372d096cff
DragonMess <kmylop@hotmail.com>
2020-01-16 00:39:20 +0000

    move done

--- play.js
+++ play.js
@@ -1,19 +1,19 @@
 const net = require('net');
+const { setupInput } = require('./input');
 
 // play.js
 const { connect } = require('./client');
 
 console.log('Connecting ...');
 
-let conn = connect();
+// let conn = 
+connect();
+setupInput();
 //put return of fonction connect in conn
 
 
-conn.on('connect', () => {
-  console.log('Successfully connected to game server');
-  // client.write('Hello there!');// data for client
-  conn.write('Camilo');
-  setInterval(() => conn.write('Move: up'), 1000);
-  setInterval(() => conn.write('Move: left'), 1500);
-});
+
+
+
+
 
